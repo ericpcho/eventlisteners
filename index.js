37bdafaad8701a8447cb47bc1dab85913bbea0a9
ericpcho <ericpcho@gmail.com>
2017-08-24 09:46:54 -0700

    added html, javascript, and css file to shopping list project

---
+++ index.js
@@ -0,0 +1,27 @@
+const STORE = [];
+
+
+function handleForm(){
+// setup event listener for submission
+  $('#js-shopping-list-form').submit(function(event){
+    const inputValue = $('.js-shopping-list-entry').val();
+  })
+// get input value
+// assign input value to variable
+// take input variable and create string
+// push to variable STORE array
+// return information from store array arr.join(" ")
+// render the string
+}
+
+function 
+
+
+function handleEventListeners() {
+  handleForm(); 
+  // set up event handler for delete button
+  // set up event listener for edit 
+}
+
+
+$(handleEventListeners); 
